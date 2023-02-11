b311be635add23879fec7022e060c1fe23d840e3
git_LinexMugambi <linexmugambi254@gmail.com>
2023-02-11 14:47:49 +0300

    passed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
-
+    long = array.size
+    case 
+        when long == 1
+            return array.join
+        when long == 2
+            lastelem = array.pop
+            array.push(" and ")
+            array.push (lastelem)
+            return array.join
+        when long >=3
+        
+            lastelem = array.pop  
+            array.push("and ")
+            cad = array.join(', ')
+            cad = cad + lastelem
+            return cad
+           
+    end
+  
 end
